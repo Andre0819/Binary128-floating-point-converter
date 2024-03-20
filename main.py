fceb73007c80c35c6e390680a2b77807dbcd65e4
Andre <andre_aquino@dlsu.edu.ph>
2024-03-20 16:11:31 +0800

    fix: added limit to mantissa, no NaN implemented

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import tkinter as tk
-
+import decimal
+import sys
+sys.set_int_max_str_digits(5000)
+decimal.getcontext().prec = 112
+Decimal = decimal.Decimal
 
 # Main Convert Logic
 def convert():
     clear_output()
-    sign_bit = ""
+    sign_bit = "0"
     # Get the selected conversion type 
     selected_conversion_type = conversion_type.get()
     
     # Get the float input from the entry widget
     if selected_conversion_type == "Binary":
-        if sign.get() == "Negative":
-            sign_bit = "1"
-        else:
-            sign_bit = "0"
         binary_input = str(input_mantissa_dec.get())
+
+        if binary_input[0] == "-":
+            sign_bit = "1"
+            binary_input = binary_input[1:]
 
         if not all(bit in '01.' for bit in binary_input) or binary_input.count('.') > 1: 
             error_message.config(text="Error: Invalid binary input.")
@@ -26,15 +30,18 @@ def convert():
             error_message.config(text="Error: Invalid exponent input. Must be integer.")
             return 0
         mantissa, exponent = normalize_binary(binary_input, exponent_input)
+        mantissa = limit_mantissa(mantissa)
+        print("normalized: ", mantissa, exponent)
         binary_convert_to_floating_point(sign_bit, mantissa, exponent)
     else:
-        float_input = float(input_mantissa_dec.get())
-        if sign.get() == "Negative":
+        float_input = str(input_mantissa_dec.get())
+        if float_input[0] == "-":
             sign_bit = "1"
-        else:
-            sign_bit = "0"
+            float_input = float_input[1:]
         exponent_input = int(exp_input.get())
         mantissa, exponent = decimal_convert_to_normalized_binary(float_input, exponent_input)
+        mantissa = limit_mantissa(mantissa)
+        print("normalized: ", len(mantissa), mantissa, exponent)
         binary_convert_to_floating_point(sign_bit, mantissa, exponent)
 
 def binary_convert_to_floating_point(sign, mantissa, exponent):
@@ -53,9 +60,14 @@ def binary_convert_to_floating_point(sign, mantissa, exponent):
     
     # Compute binary of mantissa
     if infinite_flag == False:
-        binary_mantissa = mantissa + str(bin(0)[2:].zfill(112-len(mantissa)))
+        if len(mantissa) < 112:
+            mantissa = mantissa + "0" * (112 - len(mantissa))
+            mantissa_output = mantissa + "0...0"
+        binary_mantissa = mantissa
+        print(mantissa)
+        print(len(binary_mantissa), binary_mantissa)
         # Have another mantissa variable but with ellipsis for output
-        mantissa_output = mantissa + "0...0"
+        mantissa_output = binary_mantissa
     else:
         binary_mantissa = "0" * 112
         mantissa_output = "0...0"
@@ -72,11 +84,14 @@ def binary_convert_to_floating_point(sign, mantissa, exponent):
     hex_output.config(text="0x"+hex_output_text_split.upper())
 
 def decimal_convert_to_normalized_binary(decimal, exponent):
-    # Multiply decimal by 10 raised to the exponent
-    decimal = decimal * 10**exponent
+    
+    dec = adjust_decimal(decimal, exponent)
+    print(dec)
 
+    if "." not in dec:
+        dec += ".0"
     # Split the decimal into integer and fractional parts
-    integer_part, fractional_part = str(decimal).split('.')
+    integer_part, fractional_part = str(dec).split('.')
 
     # Convert the integer part to binary
     integer_binary = bin(int(integer_part))[2:]
@@ -87,8 +102,6 @@ def decimal_convert_to_normalized_binary(decimal, exponent):
     # Combine integer and fractional binary parts
     binary = integer_binary + '.' + fraction
 
-    print(binary)
-
     # Normalize the binary 
     normalized_binary, new_exponent = normalize_binary(binary, 0)
 
@@ -97,7 +110,7 @@ def decimal_convert_to_normalized_binary(decimal, exponent):
     return normalized_binary, new_exponent
 
 def normalize_binary(binary, exponent):
-    # Find the position of the first '1' before the decimal
+    # Find the position of the first '1' before the decimal point
     dot_position = binary.find('.')
     first_one_position = binary.find('1')
     print(first_one_position, " ", dot_position)
@@ -118,7 +131,32 @@ def normalize_binary(binary, exponent):
 
     print(normalized_binary)
 
-    return normalized_binary[2:], new_exponent 
+    return normalized_binary[2:], new_exponent
+
+def adjust_decimal(decimal_str, exponent):
+    # Convert the decimal string to a Decimal object
+    decimal_value = Decimal(decimal_str)
+    print(decimal_value)
+    # Multiply the decimal value by 10^exponent
+    adjusted_value = Decimal(decimal_str) * Decimal(str(10 ** exponent))
+    
+    # Convert the adjusted value back to a string
+    adjusted_str = str(adjusted_value)
+    
+    return adjusted_str
+
+def limit_mantissa(mantissa):
+    if len(mantissa) > 112:
+        # Perform ROUND HALF TO EVEN
+        if mantissa[112:] == ("1" + "0" * (len(mantissa) - 113)) and mantissa[111] == "1":
+            mantissa = bin(int(mantissa[:112], 2) + 1)[2:]
+        elif mantissa[112] == "1":
+            mantissa = bin(int(mantissa[:112], 2) + 1)[2:]
+        else:
+            mantissa = mantissa[:112]
+        return mantissa
+    else:
+        return mantissa
 
 def fraction_to_binary(fraction, precision=112):
     binary = ""  # Initialize with "" for the fractional part
@@ -130,7 +168,6 @@ def fraction_to_binary(fraction, precision=112):
         precision -= 1
 
     return binary  
-
 
 # GUI LOGIC
 def update_label():
@@ -159,7 +196,7 @@ def clear_input():
 
 # Create the Tkinter window
 window = tk.Tk()
-window.geometry("600x400")  # Set the window size to 400x300 pixels
+window.geometry("900x400")  # Set the window size to 400x300 pixels
 
 # Create a frame for the radio buttons
 radio_frame = tk.Frame(window)
@@ -175,40 +212,40 @@ radio_binary.pack(side="left")
 radio_decimal = tk.Radiobutton(radio_frame, text="Decimal", variable=conversion_type, value="Decimal", command=update_label)
 radio_decimal.pack(side="left")
 
-sign_frame = tk.Frame(window)
-sign_frame.pack()
-inner_sign_frame = tk.Frame(sign_frame)
-inner_sign_frame.pack()
-# Create the sign option menu
-label_sign = tk.Label(inner_sign_frame, text="Sign:")
-label_sign.pack(side="top", padx=5)
-sign = tk.StringVar(inner_sign_frame)
-sign.set("Negative")
-sign_option_menu = tk.OptionMenu(inner_sign_frame, sign, "Negative", "Positive")
-sign_option_menu.pack(side="left")
+# sign_frame = tk.Frame(window)
+# sign_frame.pack()
+# inner_sign_frame = tk.Frame(sign_frame)
+# inner_sign_frame.pack()
+# # Create the sign option menu
+# label_sign = tk.Label(inner_sign_frame, text="Sign:")
+# label_sign.pack(side="top", padx=5)
+# sign = tk.StringVar(inner_sign_frame)
+# sign.set("Negative")
+# sign_option_menu = tk.OptionMenu(inner_sign_frame, sign, "Negative", "Positive")
+# sign_option_menu.pack(side="left")
 
 
 # Create a frame for the inputs
 input_frame = tk.Frame(window)
 input_frame.pack(pady=5)
 
-input_frame1 = tk.Frame(input_frame)
-input_frame1.pack(side="left", padx=10)
-input_frame2 = tk.Frame(input_frame)
-input_frame2.pack(side="left", padx=10)
+input_frame1 = tk.Frame(window)
+input_frame1.pack( padx=10)
+input_frame2 = tk.Frame(window)
+input_frame2.pack( padx=10)
 
 
 
 # Create the float input label and entry widget
 label_mantissa_dec = tk.Label(input_frame1, text="Binary:")
-label_mantissa_dec.pack(side="top")
-input_mantissa_dec = tk.Entry(input_frame1)
+label_mantissa_dec.pack(side="left")
+input_mantissa_dec = tk.Entry(input_frame1, width=100)
 input_mantissa_dec.pack(side="left")
 
 # Create the integer input label and entry widget
 label_exponent = tk.Label(input_frame2, text="Base-2 Exponent:")
-label_exponent.pack(side="top")
-exp_input = tk.Entry(input_frame2)
+label_exponent.pack(side="left")
+exp_input = tk.Entry(input_frame2, width=100)
 exp_input.pack(side="left")
 
 # Create Error message
